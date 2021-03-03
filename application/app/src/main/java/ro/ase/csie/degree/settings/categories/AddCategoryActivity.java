3f0230d76fe7462d4315d1d22bbb89eb5b3ef6ef
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-03-03 21:45:09 +0200

    load img from firebase storage to imageview

--- application/app/src/main/java/ro/ase/csie/degree/settings/categories/AddCategoryActivity.java
+++ application/app/src/main/java/ro/ase/csie/degree/settings/categories/AddCategoryActivity.java
@@ -1,32 +1,51 @@
 package ro.ase.csie.degree.settings.categories;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import ro.ase.csie.degree.R;
 
 import ro.ase.csie.degree.model.Category;
 import ro.ase.csie.degree.util.IconAdapter;
-import com.google.android.material.textfield.TextInputEditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddCategoryActivity extends AppCompatActivity {
 
     public static final String NEW_CATEGORY = "new_category";
-    private List<String> icons = new ArrayList<>();
+    private List<Bitmap> icons = new ArrayList<>();
 
     private TextInputEditText tiet_name;
     private GridView gv_icons;
     private Button btn_save;
 
     private Intent intent;
+    final long ONE_MEGABYTE = 1024 * 1024 * 1024;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,19 +53,28 @@ public class AddCategoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_category);
 
 
-        icons.add("icon_dark_mode_1");
-        icons.add("icon_dark_mode_2");
-        icons.add("icon_dark_mode_3");
-        icons.add("ic_baseline_add_circle_24");
-        icons.add("ic_baseline_add_circle_24");
-        icons.add("ic_baseline_add_circle_24");
-        icons.add("ic_baseline_add_circle_24");
-        icons.add("ic_baseline_add_circle_24");
-
 
         initComponents();
         setAdapter();
 
+        ImageView iv_test = findViewById(R.id.iv_test);
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageReference = storage.getReference().child("greens").child("green_1.png");
+
+        storageReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                Log.e("onSuccess", bmp.toString());
+                iv_test.setImageBitmap(bmp);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Toast.makeText(getApplicationContext(), "No Such file or Path found!!", Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
@@ -70,6 +98,8 @@ public class AddCategoryActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
     private View.OnClickListener saveCategoryEventListener() {
         return v -> {
--- application/app/src/main/java/ro/ase/csie/degree/util/IconAdapter.java
+++ application/app/src/main/java/ro/ase/csie/degree/util/IconAdapter.java
@@ -2,6 +2,8 @@ package ro.ase.csie.degree.util;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,23 +15,26 @@ import androidx.annotation.Nullable;
 import ro.ase.csie.degree.R;
 
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
 import android.graphics.Color;;
 
 public class IconAdapter extends ArrayAdapter<String> {
 
     private Context context;
     private int resource;
-    private List<String> iconList;
+    private List<Bitmap> iconList = new ArrayList<Bitmap>();
     private LayoutInflater layoutInflater;
 
     private int selectedIndex = 0;
 
     public IconAdapter(@NonNull Context context,
                        int resource,
-                       @NonNull List<String> iconList,
+                       @NonNull List<Bitmap> iconList,
                        LayoutInflater layoutInflater) {
-        super(context, resource, iconList);
+        super(context, resource);
         this.context = context;
         this.resource = resource;
         this.iconList = iconList;
@@ -45,7 +50,7 @@ public class IconAdapter extends ArrayAdapter<String> {
 
         if (iconList.size() > 0) {
             if (position < iconList.size()) {
-                String icon = iconList.get(position);
+                Bitmap icon = iconList.get(position);
                 if (icon != null) {
                     buildIcon(view, icon);
                 }
@@ -58,11 +63,9 @@ public class IconAdapter extends ArrayAdapter<String> {
         return view;
     }
 
-    private void buildIcon(View view, String icon) {
+    private void buildIcon(View view, Bitmap icon) {
         ImageView iv_category_icon = view.findViewById(R.id.row_item_icon);
-
-        int resID = context.getResources().getIdentifier(icon, "drawable", context.getPackageName());
-        iv_category_icon.setImageResource(resID);
+        iv_category_icon.setImageBitmap(icon);
     }
 
     public void setSelectedIndex(int selectedIndex) {
