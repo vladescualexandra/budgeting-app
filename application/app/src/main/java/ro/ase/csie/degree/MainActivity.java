ef32d1e9f8595b6aa72af59ddbbd53dd23f8865c
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-05-18 21:13:26 +0300

    fixed onBackPressed

--- application/app/src/main/java/ro/ase/csie/degree/MainActivity.java
+++ application/app/src/main/java/ro/ase/csie/degree/MainActivity.java
@@ -328,7 +328,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        finishAffinity();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
 }
